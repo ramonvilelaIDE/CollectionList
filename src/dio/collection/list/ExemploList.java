4f9bfc0ddf30e3b5ee22e68ff5cfed274bb1dd13
ramonvilelaIDE <ramonvilela92@hotmail.com>
2023-05-16 15:46:06 -0300

    aula 1 collection list

--- src/dio/collection/list/ExemploList.java
+++ src/dio/collection/list/ExemploList.java
@@ -1,6 +1,8 @@
 package dio.collection.list;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 class ExemploList {
@@ -55,7 +57,59 @@ class ExemploList {
         /*System.out.println("Exiba todas as notas na ordem em que foram informadas: ");
         for (Double nota: notas) System.out.println(nota);*/
 
+        //7)Exiba a terceira nota adicionada:
+        System.out.println("Exiba a terceira nota adicionada: " + notas.get(2));//Usamos o método '.get', então passamos a posição que desejamos dentro do método
+        System.out.println(notas.toString());//para verificar que a posição das notas
+
+        //8)Exiba a menor nota:
+        System.out.println("Exiba a menor nota: " + Collections.min(notas));//passar a colections com o método '.min' e passar a collection 'notas'como argumento desse método
+
+        //9)Exiba a maior nota:
+        System.out.println("Exiba a maior nota: " + Collections.max(notas));//'.max' para o maior elemento da collection
+
+        //10)Exiba a soma dos valores:
+        Iterator<Double> iterator = notas.iterator();//criação de variável do padrão iterator. Atalho: ctrl+alt+v
+        double soma = 0d;
+        while(iterator.hasNext()){ //laço que diz: "enquanto(iterator, tem próximo?)"
+            Double next = iterator.next();//variavel next recebe o valor do iterator.next
+            soma += next;//variavel soma para somar todos os valores recebidos pelo next
         }
+        System.out.println("Exiba a soma dos valores: " + soma);
+
+        //11)Exiba a média das notas:
+        System.out.println("Exiba a média das notas: " + (soma/notas.size()));//size é o método que retorna o valor int de elementos.
+
+        //12)Remova a nota 0.0:
+        System.out.println("Remova a nota 0.0: ");
+        notas.remove(0d);//método remove  com o objeto. Se eu colocar apenas o '0' ele removerá a posição 0.
+        System.out.println("Novas notas: " + notas);
+
+        //13)Remova a nota na posição 0:
+        System.out.println("Remova a nota na posição 0: ");
+        notas.remove(0);
+        System.out.println("Novas notas: " + notas);
+
+        //14)Remova as notas menores do que 7 e exiba a lista:
+        System.out.println("Remova as notas menores do que 7 e exiba a lista: ");
+        Iterator<Double> iterator1 = notas.iterator();
+        while(iterator1.hasNext()) {
+            double next = iterator1.next();
+            if(next < 7) iterator1.remove();
+        }
+        System.out.println(notas);
+
+        //15)Apague toda a lista:
+        System.out.println("Apague toda a lista: ");
+        notas.clear();//'notas.removeAll(notas);' foi o método que eu usei e funcionou também. Obtive como retorno '[]'
+        System.out.println(notas);
+
+        //16)Confira se a lista está vazia:
+        System.out.println("Confira se a lista está vazia: " + notas.isEmpty());//esse método retorna um boolean, sendo true == empty
+
+
+
+
+    }
 
     }
 
